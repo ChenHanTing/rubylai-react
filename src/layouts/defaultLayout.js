763d570f604c6ad83d145fd9be4b537509cdbb53
Han <han@jvdiamondtech.com>
2021-05-01 13:38:54 +0800

    feat: 側邊欄

--- src/layouts/defaultLayout.js
+++ src/layouts/defaultLayout.js
@@ -1,4 +1,5 @@
-import { useRef, useLayoutEffect, useState } from 'react';
+import React, { useRef, useLayoutEffect, useState, useEffect } from 'react';
+import { useWindowWidthAndHeight } from 'mixins/window-resize';
 import { Route } from 'react-router-dom';
 import Dropdown from './dropdown';
 
@@ -24,23 +25,38 @@ const socialIcon = [
   { icon: 'far fa-envelope', link: 'mailto:rubyfiber@gmail.com' },
 ]
 
+export const NavContext = React.createContext({
+  mobileNav: false, showMobileNav: () => {},
+})
+
 const DefaultLayout = ({ children, ...rest }) => {
+  /* useContext, useState 用法很像 */
+  const [mobileNav, showMobileNav] = useState(false)
+  const handleNavClick = () => {
+    showMobileNav(prevStatus => !prevStatus)
+    console.log('手機版導覽列狀態:', mobileNav);
+  }
   // 導覽列scroll dom
   const navRef = useRef(null);
-  // 下拉式選單點擊
-  const [dropdown, setDropdown] = useState(false);
-  const handleNavClick = () => {
-    setDropdown(!dropdown);
-  }
+  // 電腦版: mobileNav 必須為 false
+  const [width, height] = useWindowWidthAndHeight();
+  // 並利用useEffect 偵測寬度
+  useEffect(() => {
+    if(width < 992) return
 
+    showMobileNav(() => false)
+  }, [width])
+
+  /**
+   * 與宣告 useEffect 本身相同，但它會在所有 DOM 改變後，同步調用。
+   * 使用它來讀取 DOM layout 並同步重新 render。在瀏覽器執行繪製之前，useLayoutEffect 內部的更新將被同步刷新。
+   */
   useLayoutEffect(() => {
     const handleScroll = () => {
       // 導覽列的距離
-      const navPosition = navRef.current.getBoundingClientRect().top
+      const navPosition = navRef.current.getBoundingClientRect()
       // 滾輪距離 (目前用不到)
       const scrollPosition = window.scrollY + window.innerHeight
-
-      console.log('scrollPosition:', window.scrollY);
 
       if(window.scrollY > 20) {
         navRef.current.classList.add('sticky');
@@ -52,6 +68,9 @@ const DefaultLayout = ({ children, ...rest }) => {
     window.addEventListener('scroll', handleScroll)
     return () => window.removeEventListener('scroll', handleScroll)
   }, [])
+
+  /* 漢堡選單狀態 */
+  const hamburgerStatus = mobileNav ? 'fas fa-times' : 'fas fa-bars'
 
   return (
     <>
@@ -69,8 +88,10 @@ const DefaultLayout = ({ children, ...rest }) => {
           </div>
           {/* 手機版 */}
           <div className="d-lg-none nav-items-mobile">
-            <i className="fas fa-bars"></i>
-            {/*<Dropdown childrenItems={navItem} />*/}
+            <NavContext.Provider value={mobileNav}>
+              <i className={hamburgerStatus} onClick={handleNavClick}></i>
+              {mobileNav && <Dropdown childrenItems={navItem} handleClick={handleNavClick} />}
+            </NavContext.Provider>
           </div>
         </nav>
         <article>{children}</article>
--- src/layouts/dropdown.js
+++ src/layouts/dropdown.js
@@ -1,23 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function Dropdown({ childrenItems }) {
-  const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
+function Dropdown({ childrenItems, handleClick }) {
+  const [dropdownHeight, setDropdownHeight] = useState(122)
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      /* TODO: debounce */
+      setDropdownHeight((window.scrollY > 20) ? 84 : 122)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   const eachItem = (item, index) =>
     <li key={index}>
       {/* 選單選完即關掉選單 */}
-      <Link className='dropdown-link' to={item.link}
-            onClick={() => setClick(false)}>
+      <Link className='dropdown-link' to={item.link} onClick={handleClick}>
         {item.name}
       </Link>
     </li>
 
   return (
-    <ul onClick={handleClick}
-        className={click ? 'custom-dropdown-menu d-none' : 'custom-dropdown-menu'}
-    >
+    <ul className='custom-dropdown-menu' ref={dropdownRef} style={{top: dropdownHeight}}>
       {childrenItems.map((item, index) => {
         return eachItem(item, index);
       })}
